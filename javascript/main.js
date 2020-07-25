0f74e39f0f00c9d10195f080f7cfe81aa31550dd
sahitha5667 <sahithajayam1@gmail.com>
2020-07-25 16:01:24 +0530

    resume

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-function loadjson(file,callback){
+/*function loadjson(file,callback){
 	var xhr=new XMLHttpRequest();
 	xhr.overrideMimeType("application/json");
 	xhr.open("GET",file,true);
@@ -13,7 +13,29 @@ loadjson("data.json",function(text){
 	let data=JSON.parse(text);
 	console.log(data);
 	basic(data.details);
-})
+	careerinfo(data.careerobjective);
+	education(data.educationqualification);
+	skills(data.technicalskills;
+})*/
+
+
+function loadjson(file){
+	return new Promise((resolve,reject)=>{
+		return fetch(file).then(response=>{
+			if(response.ok){
+				resolve(response.json());
+			}
+			else{
+				reject(new Error('error'));
+			}
+		})
+	})
+}
+
+var newfile=loadjson("data.json");
+newfile.then(data=>{
+	console.log(data);
+}) 
 
 var child1=document.querySelector(".child1");
 
@@ -46,6 +68,56 @@ function basic(det){
 	address1.textContent=det.address;
 	child1.appendChild(address1);
 
-	
+}
 
+var child2=document.querySelector(".child2");
+
+function careerinfo(info1){
+	var heading1=document.createElement("h2");
+	heading1.textContent="careerobjective:";
+	child2.appendChild(heading1);
+
+	child2.appendChild(document.createElement("hr"));
+
+	var heading2=document.createElement("p");
+	heading2.textContent=info1.info;
+	child2.appendChild(heading2);
+}
+
+function education(edu){
+	var heading3=document.createElement("h2");
+	heading3.textContent="Educational Qualification";
+	child2.appendChild(heading3);
+
+	child2.appendChild(document.createElement("hr"));
+
+	var table1=document.createElement("table");
+	table1.border="1";
+	child2.appendChild(table1);
+
+	tabledata="";
+	for(i=0;i<edu.length;i++){
+		tabledata+="<tr><td>"+edu[i].institute+"</td><td>"+edu[i].degree+"</td><td>"+edu[i].passedoutyear+"</td><td>"+edu[i].percentage+"</td></tr>";	
+
+	}
+	table1.innerHTML=tabledata;
+}
+function skills(skillinfo){
+	var hd=document.createElement("h2");
+	hd.textContent="technicalskills";
+	child2.appendChild(hd);
+
+	child2.appendChild(document.createElement("hr"));
+
+	for(i=0;i<skillinfo.length;i++){
+		var title=document.createElement("h4");
+		title.textContent=skillinfo[i].title;
+		child2.appendChild(title);
+
+		var skillul=document.createElement("ul");
+		var skillli=document.createElement("li");
+		skillli.textContent=skillinfo[i].info;
+		skillul.appendChild(skillli);
+		child2.appendChild(skillul);
+	}
 }
